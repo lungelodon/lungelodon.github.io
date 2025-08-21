1f4feefc85782566c09872c66cc276bc96f03862
Lungelo Don <155607281+lungelodon@users.noreply.github.com>
2025-08-21 08:41:57 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:  “/”,
+  base:  '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
